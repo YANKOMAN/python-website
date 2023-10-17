a28069b31c921cd0d9c3b3598c6e3c00025390d7
YANKOMAN <89599839+YANKOMAN@users.noreply.github.com>
2023-10-17 15:06:36 +0000

    add database

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS= [
+{
+  'id' : 1,
+  'title' : 'full stack devloper',
+  'location' : 'New York',
+  'salary' : '100000'
+},
+  {
+    'id' : 2,
+    'title' : 'web devloper',
+    'location' : 'casablanca',
+    'salary' : '200000'
+  }
+  ]
+
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
